a9fb50ce29ee3024c5098d9bf1cc986756e049f1
digitalgrid123 <harisrajpoot196@gmail.com>
2024-05-10 20:57:48 +0500

    add

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,6 +4,7 @@ const Menu = () => {
   const [activeSection, setActiveSection] = useState("about");
   const [isMobile, setIsMobile] = useState(false);
   const [menuHeight, setMenuHeight] = useState("auto");
+  const [gap, setGap] = useState(0);
   const menuRef = useRef(null);
 
   // Function to check if the screen size is mobile
@@ -17,17 +18,27 @@ const Menu = () => {
     setMenuHeight(`${screenHeight}px`);
   };
 
+  // Calculate gap based on the height of the screen
+  const calculateGap = () => {
+    const menuItemHeight = menuRef.current.querySelector("li").offsetHeight;
+    const gapValue = (window.innerHeight - menuItemHeight) / 2;
+    setGap(gapValue);
+  };
+
   // Add event listeners to check screen size and set menu height on component mount and resize
   useEffect(() => {
     checkIsMobile();
     setSideMenuHeight();
+    calculateGap();
 
     window.addEventListener("resize", checkIsMobile);
     window.addEventListener("resize", setSideMenuHeight);
+    window.addEventListener("resize", calculateGap);
 
     return () => {
       window.removeEventListener("resize", checkIsMobile);
       window.removeEventListener("resize", setSideMenuHeight);
+      window.removeEventListener("resize", calculateGap);
     };
   }, []);
 
@@ -59,84 +70,77 @@ const Menu = () => {
             activeSection === "about" ? "active" : ""
           } ${isMobile && activeSection !== "about" ? "centered" : ""}`}
           onClick={() => scrollToSection("about")}
+          style={{ gap: activeSection === "about" ? `${gap}px` : 0 }}
         >
           <h6 className="roman-text">I</h6>
-
           {isMobile && activeSection === "about" && (
             <h6 className="navigate-heading">About</h6>
           )}
-
           {!isMobile && <h6 className="navigate-heading">About</h6>}
         </li>
         <li
           className={`bg-orange navigation ${
             activeSection === "pain-points" ? "active" : ""
-          }${isMobile && activeSection !== "pain-points" ? "centered" : ""}`}
+          } ${isMobile && activeSection !== "pain-points" ? "centered" : ""}`}
           onClick={() => scrollToSection("pain-points")}
+          style={{ gap: activeSection === "pain-points" ? `${gap}px` : 0 }}
         >
           <h6 className="roman-text">II</h6>
-
           {isMobile && activeSection === "pain-points" && (
             <h6 className="navigate-heading">Pain points</h6>
           )}
-
           {!isMobile && <h6 className="navigate-heading">Pain points</h6>}
         </li>
         <li
           className={`bg-green navigation ${
             activeSection === "new-approach" ? "active" : ""
-          }${isMobile && activeSection !== "new-approach" ? "centered" : ""}`}
+          } ${isMobile && activeSection !== "new-approach" ? "centered" : ""}`}
           onClick={() => scrollToSection("new-approach")}
+          style={{ gap: activeSection === "new-approach" ? `${gap}px` : 0 }}
         >
           <h6 className="roman-text">III</h6>
-
           {isMobile && activeSection === "new-approach" && (
             <h6 className="navigate-heading">New approach</h6>
           )}
-
           {!isMobile && <h6 className="navigate-heading">New approach</h6>}
         </li>
         <li
           className={`bg-black navigation ${
             activeSection === "products" ? "active" : ""
-          }${isMobile && activeSection !== "products" ? "centered" : ""}`}
+          } ${isMobile && activeSection !== "products" ? "centered" : ""}`}
           onClick={() => scrollToSection("products")}
+          style={{ gap: activeSection === "products" ? `${gap}px` : 0 }}
         >
           <h6 className="roman-text">IV</h6>
-
           {isMobile && activeSection === "products" && (
             <h6 className="navigate-heading">Products</h6>
           )}
-
           {!isMobile && <h6 className="navigate-heading">Products</h6>}
         </li>
         <li
           className={`bg-yellow navigation ${
             activeSection === "benefits" ? "active" : ""
-          }${isMobile && activeSection !== "benefits" ? "centered" : ""}`}
+          } ${isMobile && activeSection !== "benefits" ? "centered" : ""}`}
           onClick={() => scrollToSection("benefits")}
+          style={{ gap: activeSection === "benefits" ? `${gap}px` : 0 }}
         >
           <h6 className="roman-text">V</h6>
-
           {isMobile && activeSection === "benefits" && (
             <h6 className="navigate-heading">Benefits</h6>
           )}
-
           {!isMobile && <h6 className="navigate-heading">Benefits</h6>}
         </li>
-
         <li
           className={`bg-purple navigation ${
             activeSection === "contact-us" ? "active" : ""
-          }${isMobile && activeSection !== "contact-us" ? "centered" : ""}`}
+          } ${isMobile && activeSection !== "contact-us" ? "centered" : ""}`}
           onClick={() => scrollToSection("contact-us")}
+          style={{ gap: activeSection === "contact-us" ? `${gap}px` : 0 }}
         >
-          <h6 className="roman-text"> VI</h6>
-
+          <h6 className="roman-text">VI</h6>
           {isMobile && activeSection === "contact-us" && (
             <h6 className="navigate-heading">Contact us</h6>
           )}
-
           {!isMobile && <h6 className="navigate-heading">Contact us</h6>}
         </li>
       </ul>
